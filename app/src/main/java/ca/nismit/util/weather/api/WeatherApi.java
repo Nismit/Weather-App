818e18f89608d0e96ad446c00037cbacdaf2a325
Nismit <nismit.dev@gmail.com>
2017-03-14 14:36:22 -0700

    Added forecast in api interface

--- app/src/main/java/ca/nismit/util/weather/api/WeatherApi.java
+++ app/src/main/java/ca/nismit/util/weather/api/WeatherApi.java
@@ -1,5 +1,6 @@
 package ca.nismit.util.weather.api;
 
+import ca.nismit.util.weather.pojoForecast.WeatherForecastResponse;
 import ca.nismit.util.weather.pojoWeather.WeatherResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -17,6 +18,21 @@ public interface WeatherApi {
 
     @GET("/{urlPath}")
     Call<WeatherResponse> getWeatherWithGPS(
+            @Path("urlPath") String urlPath,
+            @Query("lat") float lat,
+            @Query("lon") float lon,
+            @Query("appid") String appId
+    );
+
+    @GET("/{urlPath}")
+    Call<WeatherForecastResponse> getForecastWithCityID(
+            @Path("urlPath") String urlPath,
+            @Query("id") String cityId,
+            @Query("appid") String appId
+    );
+
+    @GET("/{urlPath}")
+    Call<WeatherForecastResponse> getForecastWithGPS(
             @Path("urlPath") String urlPath,
             @Query("lat") float lat,
             @Query("lon") float lon,
