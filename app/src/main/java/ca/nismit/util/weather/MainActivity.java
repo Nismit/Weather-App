7c5656425d8109fe8673c4063b5a554250af1fe7
Nismit <nismit.dev@gmail.com>
2017-03-13 18:14:51 -0700

    Added Transparent Status bar with no Action bar

--- app/src/main/java/ca/nismit/util/weather/MainActivity.java
+++ app/src/main/java/ca/nismit/util/weather/MainActivity.java
@@ -3,6 +3,7 @@ package ca.nismit.util.weather;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
@@ -24,7 +25,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //
+        // Transparent Status bar with no Action bar
+        //
+        findViewById(android.R.id.content).setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
         setContentView(R.layout.activity_main);
+
         textView = (TextView) findViewById(R.id.sample);
 
         WeatherApi apiInterface = ClientHelper.createService(WeatherApi.class);
@@ -36,10 +45,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Response Code: " + response.code());
                 WeatherResponse resource = response.body();
                 Log.d(TAG, "NAME: " + resource.getName());
+                String temp = Double.toString((resource.getMain().getTemp() - 273.15f));
                 List<Weather> weather = resource.getWeather();
                 Log.d(TAG, "DESC: " + weather.get(0).getDescription());
 
-                textView.setText("PLACE: " + resource.getName() + "\nWEATHER: " + weather.get(0).getDescription());
+                textView.setText("PLACE: " + resource.getName() + "\nWEATHER: " + weather.get(0).getDescription() +"\nTEMP: " + temp);
             }
 
             @Override
