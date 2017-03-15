44908cda03bba8f7be9e2e976030d745e1971af9
Nismit <nismit.dev@gmail.com>
2017-03-15 14:48:49 -0700

    Chart Example

--- app/src/main/java/ca/nismit/util/weather/MainActivity.java
+++ app/src/main/java/ca/nismit/util/weather/MainActivity.java
@@ -1,12 +1,28 @@
 package ca.nismit.util.weather;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.nismit.util.weather.api.WeatherApi;
@@ -26,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView _textLocation;
     private TextView _textDate;
     private TextView _textTemp;
+    private CombinedChart _mChart;
+    private final int itemcount = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +60,72 @@ public class MainActivity extends AppCompatActivity {
         _textLocation = (TextView) findViewById(R.id.ac_textLocation);
         _textDate = (TextView) findViewById(R.id.ac_textDate);
         _textTemp = (TextView) findViewById(R.id.ac_textTemp);
+        _mChart = (CombinedChart) findViewById(R.id.ac_combiChart);
+
+        //
+        // Init chart
+        //
+        _mChart.getDescription().setEnabled(false);
+        _mChart.setBackgroundColor(Color.WHITE);
+        _mChart.setDrawGridBackground(false);
+        _mChart.setDrawBarShadow(false);
+        _mChart.setHighlightFullBarEnabled(false);
+        _mChart.setVisibleXRangeMaximum(5);
+
+        // draw bars behind lines
+        _mChart.setDrawOrder(new CombinedChart.DrawOrder[]{
+                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE
+        });
+
+        Legend l = _mChart.getLegend();
+        l.setWordWrapEnabled(true);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+
+        YAxis rightAxis = _mChart.getAxisRight();
+        rightAxis.setDrawGridLines(true);
+        rightAxis.setDrawLabels(true);
+        rightAxis.setDrawZeroLine(true);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis leftAxis = _mChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        //leftAxis.setDrawLabels(false);
+        leftAxis.setDrawLabels(true);
+        leftAxis.setAxisMinimum(-10f); // this replaces setStartAtZero(true)
+
+        XAxis xAxis = _mChart.getXAxis();
+        //xAxis.setEnabled(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        //xAxis.setCenterAxisLabels(true);
+        xAxis.setAxisMinimum(-0.3f);
+        //xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mMonths[(int) value % mMonths.length];
+            }
+        });
+
+        CombinedData data = new CombinedData();
+
+        data.setData(generateLineData());
+        data.setData(generateBarData());
+
+        xAxis.setAxisMaximum(data.getXMax() + 0.25f);
+
+        _mChart.animateY(2500);
+
+        _mChart.setData(data);
+        _mChart.invalidate();
 
         apiInterface = ClientHelper.createService(WeatherApi.class);
 
-        callWeatherApi();
-        callForecastApi();
+        //callWeatherApi();
+        //callForecastApi();
     }
 
     private void callWeatherApi() {
@@ -84,7 +163,8 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<WeatherForecastResponse> call, Response<WeatherForecastResponse> response) {
                 Log.d(TAG, "callForecastApi/Response Code: " + response.code());
                 WeatherForecastResponse resource = response.body();
-                
+
+
                 List<ca.nismit.util.weather.pojoForecast.List> list = resource.getList();
                 //Log.d(TAG, "#####onResponse: " + list.size());
             }
@@ -108,4 +188,69 @@ public class MainActivity extends AppCompatActivity {
     private void setTemperature(String temperature) {
         _textTemp.setText(temperature + "°");
     }
+
+    private LineData generateLineData() {
+
+        LineData d = new LineData();
+
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+
+        for (int index = 0; index < 4; index++) {
+            //entries.add(new Entry(index, getRandom(15, 5)));
+            entries.add(new Entry(index, (-2 + index)));
+        }
+
+        entries.add(new Entry(4, 18));
+
+        LineDataSet set = new LineDataSet(entries, "Line DataSet");
+        set.setColor(Color.rgb(240, 238, 70));
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.rgb(240, 238, 70));
+        set.setCircleRadius(5f);
+        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(240, 238, 70));
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        d.addDataSet(set);
+
+        return d;
+    }
+
+    private BarData generateBarData() {
+
+        ArrayList<BarEntry> entries1 = new ArrayList<BarEntry>();
+
+        for (int index = 0; index < itemcount; index++) {
+            entries1.add(new BarEntry(index, getRandom(25, 25)));
+        }
+
+        BarDataSet set1 = new BarDataSet(entries1, "Bar 1");
+        set1.setColor(Color.rgb(60, 220, 78));
+        set1.setDrawValues(false);
+        //set1.setValueTextColor(Color.rgb(60, 220, 78));
+        //set1.setValueTextSize(10f);
+        set1.setAxisDependency(YAxis.AxisDependency.RIGHT);
+
+        float barWidth = 0.20f; // x2 dataset
+        // (0.45 + 0.02) * 2 + 0.06 = 1.00 -> interval per "group"
+
+        BarData d = new BarData(set1);
+        d.setBarWidth(barWidth);
+
+        // make this BarData object grouped
+        //d.groupBars(0, groupSpace, barSpace); // start at x = 0
+
+        return d;
+    }
+
+    protected float getRandom(float range, float startsfrom) {
+        return (float) (Math.random() * range) + startsfrom;
+    }
+
+    protected String[] mMonths = new String[] {
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
+    };
 }
